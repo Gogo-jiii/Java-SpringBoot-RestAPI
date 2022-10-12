5ac2ff7578bc9715b9c3f7260ab25de549b7cd1d
VgionTech <:(0$t\U9i9b.{3k*>
2022-10-12 17:58:09 +0530

    list null error solved

--- src/main/java/com/example/demo/service/BookService.java
+++ src/main/java/com/example/demo/service/BookService.java
@@ -1,5 +1,6 @@
 package com.example.demo.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,13 +21,14 @@ public class BookService {
 		return instance;
 	}
 
+	private static List<BookModel> list = new ArrayList<>();
+
 	private static void init() {
 		list.add(new BookModel(1, "Vaibhav 1", "Kalyan 1"));
 		list.add(new BookModel(1, "Vaibhav 2", "Kalyan 2"));
 		list.add(new BookModel(1, "Vaibhav 3", "Kalyan 3"));
 	}
 
-	private static List<BookModel> list;
 
 	public List<BookModel> getBooksFromDatabase() {
 		return list;
